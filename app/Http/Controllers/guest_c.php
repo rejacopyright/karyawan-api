de5730265b81d35b0321f21ebb883e6ce0248ae6
rejajamil <reja.copyright@gmail.com>
2020-04-15 02:28:55 +0700

    ---

--- app/Http/Controllers/guest_c.php
+++ app/Http/Controllers/guest_c.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Storage;
-use Str;
-use Image;
+use App\face;
 use App\User;
+use App\user_image as img;
+use Carbon\Carbon;
 
 class guest_c extends Controller
 {
   function image_data(){
-    // if (!is_dir('public/img/temp/absen')) { mkdir('public/img/temp/absen', 0777, true); }
-    // if ($data->image) {
-    //   Image::make($data->image)->resize(150, null, function($c){$c->aspectRatio();})->save('public/img/temp/absen/image.jpg', 50);
-    // }
-    $path = base64_encode(file_get_contents(url('public/img/capture1.jpg')));
-    return $path;
+      $image = base64_encode(file_get_contents(url('public/img/capture1.jpg')));
+    return $image;
   }
   function absen(Request $data){
-    return User::orderBy('created')->paginate(10);
+    $absen = face::whereDate('created_at', Carbon::today())->orderBy('created_at', 'DESC')->paginate(10)->map(function($i){
+      $usr = User::where('id', $i->user_id)->first();
+      $i['user'] = $usr;
+      $i['img'] = img::where('user_id', $usr->user_id)->orderBy('created_at', 'DESC')->first()->name;
+      return $i;
+    });
+    
+    // $absen = ($absen->toArray());
+    $absen_id = face::whereDate('created_at', Carbon::today())->distinct('user_id')->pluck('user_id')->all();
+    $belum = User::whereNotIn('id', $absen_id)->paginate(10)->map(function($r){
+      $r['img'] = img::where('user_id', $r->user_id)->orderBy('created_at', 'DESC')->first()->name;
+      return $r;
+    });
+    $count = face::whereDate('created_at', date('Y-m-d'))->count();
+    return compact('absen', 'belum', 'count');
   }
 }
